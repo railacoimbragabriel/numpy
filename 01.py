23695e8e31dc05dad294f64f4e1ec647c96477d1
Raíla <railacgabriel@yahoo.com>
2023-09-30 13:16:12 -0300

    first commit

---
+++ 01.py
@@ -0,0 +1,9 @@
+# Importando a biblioteca numpy e atribuindo um apelido a ela.
+import numpy as np
+
+# Criando um array Numpy:
+# Usando a função array.
+array = np.array([0, 1, 2, 3, 4, 5])
+# Mostrará apenas os elementos, lembrará muito uma lista.
+print("Array:", array)
+print(type(array))  # Mostrando que é do tipo array e não list.
